0235234b40a35c8698359ee93d71018aa0c1ad72
deepak-karkala <dkarkala01@gmail.com>
2016-09-02 16:02:39 +0200

    Corrected relative paths

--- dispBookList.php
+++ dispBookList.php
@@ -2,7 +2,9 @@
   echo "<h4>Sorted by: &nbsp &nbsp" . $_POST['sort-options'] . "</h4>";
 
   //require_once('functions.php');
-  //require_once(__DIR__.'/functions.php');
+  require_once(__DIR__.'/functions.php');
+
+  /*
   $dbhost = 'us-cdbr-iron-east-04.cleardb.net';
   $dbname = 'heroku_a6277021f19b465';
   $dbuser = 'bedaefb56bd440';
@@ -20,7 +22,7 @@
     if (!result) die($connection->error);
     return $result;
   }
-
+  */
 
   
   $query = "SELECT * FROM books";
