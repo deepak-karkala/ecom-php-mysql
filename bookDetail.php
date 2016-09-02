616ca57abe1536e8d3db6e8345d2661519db5763
deepak-karkala <dkarkala01@gmail.com>
2016-09-02 16:26:09 +0200

    Corrected relative paths

--- bookDetail.php
+++ bookDetail.php
@@ -2,6 +2,25 @@
   require_once(__DIR__.'/header.php');
   //require_once(__DIR__.'/functions.php');
 
+  	$dbhost = 'us-cdbr-iron-east-04.cleardb.net';
+	$dbname = 'heroku_a6277021f19b465';
+	$dbuser = 'bedaefb56bd440';
+	$dbpass = '9342281a';
+
+	$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+	if ($connection->connect_error) die($connection->connect_error);
+
+	function queryMysql($query)
+	{
+		global $connection;
+		$result = $connection->query($query);
+		if (!result) die($connection->error);
+		return $result;
+	}
+
+
+
+
   $isbn = $_GET['isbn'];
 
   $query = "SELECT * FROM books WHERE isbn=$isbn";
