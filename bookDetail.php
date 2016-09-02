5302f81b799b4d8ff2045bf3a816192e19c1aa86
deepak-karkala <dkarkala01@gmail.com>
2016-09-02 16:23:15 +0200

    Corrected relative paths

--- bookDetail.php
+++ bookDetail.php
@@ -1,6 +1,6 @@
 <?php
-  require_once(__DIR__.'/header.php');
-  require_once(__DIR__.'/functions.php');
+  //require_once(__DIR__.'/header.php');
+  //require_once(__DIR__.'/functions.php');
 
   $isbn = $_GET['isbn'];
 
